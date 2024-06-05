8aefd55e8a5fff36bf59e50f9b98305d54312f3b
superlolipower <157352638+superlolipower@users.noreply.github.com>
2024-06-06 08:49:15 +0900

    Update MyQueue.java

--- MyQueue.java
+++ MyQueue.java
@@ -14,38 +14,41 @@ class Queue {
   int items[] = new int[queueLength];
   int front = -1;
   int back = -1;
-  boolean isFull(){
+    //Проверка полноты очереди
+  boolean isFull(){ 
       if(back == queueLength - 1){
           return true;
       } else {
           return false;
       }
   }
+    // Функция проверки пустоты очереди
   boolean isEmpty(){
-      if(front == -1 && back == -1){
+      if(front == -1 && back == -1){ //Если значения front и back имеют значения по умолчанию, значит очередь пуста
           return true;
       } else {
           return false;
       }
   }
+    //Метод добавления элементо в очередь
    void enQueue(int itemValue) {
       if(isFull()){
           System.out.println("Queue is full");
-      } else if(front == -1 && back == -1){
+      } else if(front == -1 && back == -1){ //Если очередь пуста
           front = back = 0;
-          items[back] = itemValue;
+          items[back] = itemValue; //Присваиваем новому элементу последнее место 
       } else{
-          back++;
+          back++; //Увеличиваем счетчик на 1 вставляем элемент на последнее место
           items[back] = itemValue;
       }
   }
    void deQueue(){
       if(isEmpty()){
           System.out.println("Queue is empty. Nothing to dequeue");
-      } else if (front == back){
+      } else if (front == back){ //Если в очереди один элемент, просто сбрасываем значения front и back
           front = back = -1;
       } else {
-          front++;
+          front++; //Перемещаем front на одну ячейку
       }
   }
   void display(){
